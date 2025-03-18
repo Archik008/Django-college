aab35833572cc33bbe7f5956c93aa159742a097e
Артем <119063410+Archik008@users.noreply.github.com>
2025-03-18 20:03:30 +0500

    Laba 2

--- col_proj/first_app/models.py
+++ col_proj/first_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ExampleModel(models.Model):
+    name = models.CharField("Имя", max_length=200)
+    description = models.TextField("Описание")
+    photo = models.ImageField("Фотография", upload_to="photos/%Y/%m/%d")
+
+    def __str__(self):
+        return self.name
